e92886bbdb1a139f1b744de316fb69a111fa0bef
210120116075 <210120116075@git.org.in>
2024-03-01 23:55:12 +0530

    ada1

--- Controller/AdaController.js
+++ Controller/AdaController.js
@@ -3,8 +3,19 @@ const AdaSchema = require("../Schema/AdaSchema");
 const updateAda = async (req,res) =>{
     const sub = req.params.ada;
     // const updatedAda  = await AdaSchema.findByIdAndUpdate(id,{attendanceStatus:true})
-    const id  = await AdaSchema.findOne({studentId : sub})
-    const updatedAdasub  = await AdaSchema.findByIdAndUpdate(id,{attendanceStatus:true})
+    // const id  = await AdaSchema.findOne({studentId : sub})
+    // const updatedAdasub  = await AdaSchema.findByIdAndUpdate(id,{attendanceStatus:true})
+    const ada = {
+        studentId : sub,
+        attendanceStatus: true
+    }
+    const adaSub = new AdaSchema(ada);
+    adaSub.save().then((data)=>{
+        res.status(200).json({
+            message : "success"
+        })
+    })
+    
 
     res.status(200).json({
         message : "success"
