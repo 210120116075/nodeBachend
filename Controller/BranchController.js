1b139b91a892480138b1580b30d5eba793e8ac60
210120116075 <210120116075@git.org.in>
2024-03-02 22:55:22 +0530

    getBranch

--- Controller/BranchController.js
+++ Controller/BranchController.js
@@ -1,19 +1,33 @@
 const branchSchema = require('../Schema/BranchSchema');
 
-const addBranch = async (req,res)=>{
-    const branch =new branchSchema({branchName : req.body.branchName});
-    branch.save().then((data)=>{
+const addBranch = async (req, res) => {
+    const branch = new branchSchema({ branchName: req.body.branchName });
+    branch.save().then((data) => {
         res.status(200).json({
-            message : "branch add successfully",
-            data : data
+            message: "branch add successfully",
+            data: data
         })
-    }).catch((err)=>{
+    }).catch((err) => {
         res.status(500).json({
-            message : "error",
-            errpr : err
+            message: "error",
+            errpr: err
         })
     })
 }
+const getAllBranch = async (req, res) => {
+    const Branch = await branchSchema.find();
+    if (Branch) {
+        res.status(200).json({
+            message: "Branch fetch successfully",
+            data: Branch
+        });
+    } else {
+        res.status(500).json({
+            message: "Error in fetching branches",
+        });
+    }
+}
 module.exports = {
-    addBranch
+    addBranch,
+    getAllBranch
 }
--- Routes/BranchRoutes.js
+++ Routes/BranchRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const BranchController = require('../Controller/BranchController');
 
 router.post('/add',BranchController.addBranch);
+router.post('/getAll',BranchController.getAllBranch);
 
 module.exports = router;
