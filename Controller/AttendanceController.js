aaa07489a4fd104f17a7af946c7e2b4e36f516f1
210120116075 <210120116075@git.org.in>
2024-03-03 12:34:20 +0530

    com

--- Controller/AttendanceController.js
+++ Controller/AttendanceController.js
@@ -1,37 +1,41 @@
 const attendanceSchema = require('../Schema/AttendanceSchema');
 const qrSchema = require('../Schema/QRSchema');
 
-const addAttendance = async (req,res)=>{
+const addAttendance = async (req, res) => {
     // var id =await req.params.id;
     // var qrText1 =await req.params.qrText;
-    var id =await req.body.id;
-    var qrText1 =await req.body.qrText;
-    console.log("id : "+id);
-    console.log("qrText : "+qrText1);
-    const qrData =await qrSchema.find({qrText:qrText1},{qrStatus:"generated"});
-    if(qrData){
+    var id = await req.body.id;
+    var qrText1 = await req.body.qrText;
+    console.log("id : " + id);
+    console.log("qrText : " + qrText1);
+    const qrData1 = await qrSchema.find({
+        'qrText':qrText1,
+        'qrStatus':"generated"
+    });
+    // console.log("qr :", qrData1[0].branch);
+    if (qrData1) {
         const attendanceObj = {
-            branch: qrData.branch,
-            sem: qrData.sem,
-            subject: qrData.subject,
+            branch: qrData1[0].branch,
+            sem: qrData1[0].sem,
+            subject: qrData1[0].subject,
             studentId: id,
-            attendanceStatus : "present"
+            attendanceStatus: "present"
         }
         const attendance = new attendanceSchema(attendanceObj);
-        attendance.save().then((data)=>{
+        attendance.save().then((data) => {
             res.status(200).json({
                 message: "attendance fill successfully",
-                data:id
+                data: id
             });
-        }).catch((err)=>{
+        }).catch((err) => {
             res.status(500).json({
                 message: "error",
-                error : err
+                error: err
             });
         })
     }
 
 }
-module.exports={
+module.exports = {
     addAttendance
 }
