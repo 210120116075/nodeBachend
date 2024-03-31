b80387940f357099620fe41fe34ded12ddba0c3a
210120116075 <210120116075@git.org.in>
2024-04-01 01:20:21 +0530

    socket

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const StudentSchema = require('./Schema/StudentSchema')
-
 const cors = require("cors");
 const app = express();
 const PORT = 3001;
+const http = require('http');
+const server = http.createServer(app);
 
 app.use(express.json());
+
+const {Server} = require("socket.io");
+const socketIO = require("socket.io");
+
+const io = socketIO(server, {
+    cors: {
+        origin: "http://localhost:3001", // Adjust with your React app's URL
+        methods: ["GET", "POST"],
+    },
+});
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 app.use(cors());
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('user-message', (msg) => {
+        console.log("msg : ", msg);
+        socket.emit('user-message', msg);
+    })
+})
+
 const studentRoutes = require('./Routes/StudentRoutes');
 const facultyRoutes = require('./Routes/FacultyRoutes');
 const branchRoutes = require('./Routes/BranchRoutes');
@@ -15,17 +43,17 @@ const semRoutes = require('./Routes/SemRoutes');
 const subjectRoutes = require("./Routes/SubjectRoutes");
 const qrRoutes = require("./Routes/QRRoutes");
 const attendanceRoutes = require("./Routes/attendanceRoutes1");
-app.use('/student',studentRoutes);
-app.use('/faculty',facultyRoutes);
-app.use('/branch',branchRoutes);
-app.use('/sem',semRoutes);
-app.use('/subject',subjectRoutes);
-app.use('/qrcode',qrRoutes);
-app.use('/attendance',attendanceRoutes);
+app.use('/student', studentRoutes);
+app.use('/faculty', facultyRoutes);
+app.use('/branch', branchRoutes);
+app.use('/sem', semRoutes);
+app.use('/subject', subjectRoutes);
+app.use('/qrcode', qrRoutes);
+app.use('/attendance', attendanceRoutes);
 
-mongoose.connect("mongodb+srv://samir:samir@cluster0.key63fx.mongodb.net/vedantattandace", {
+// mongoose.connect("mongodb+srv://samir:samir@cluster0.key63fx.mongodb.net/vedantattandace", {
 // mongoose.connect("mongodb://127.0.0.1:27017/cw1", {
-
+mongoose.connect("mongodb+srv://vedantpatel:vedant@cluster0.uvqub21.mongodb.net/vedantattandace", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
@@ -35,7 +63,7 @@ mongoose.connect("mongodb+srv://samir:samir@cluster0.key63fx.mongodb.net/vedanta
 })
 
 app.listen(PORT, () => {
-    console.log("server started on port",PORT);
+    console.log("server started on port", PORT);
 });
 
 
